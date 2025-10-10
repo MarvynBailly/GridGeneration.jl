ba9bdd2db3f2e0d16028431ef8ae7a2a5fccd7ec
Marvyn B <marvyn@marvyn.com>
2025-10-09 23:48:41 -0400

    removed

--- scripts/setup_docs.jl
+++
@@ -1,7 +0,0 @@
-import Pkg
-pkgpath = abspath(joinpath(@__DIR__, ".."))
-println("Developing local package from: ", pkgpath)
-Pkg.develop(Pkg.PackageSpec(path=pkgpath))
-Pkg.instantiate()
-Pkg.precompile()
-println("Docs project setup complete")
