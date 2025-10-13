870e2f87c84f7b38f736d6bc8423ab84c4da9e42
Marvyn B <marvyn@marvyn.com>
2025-10-13 12:52:37 -0400

    metric work

--- src/metric/Metric.jl
+++ src/metric/Metric.jl
@@ -23,13 +23,40 @@ function setup_metric_tree(data)
     return tree, refs
 end
 
+function setup_metric_tree(blocks::Array{Array{Float64,3},1})
+    refs = PtRef[]
+    coords = Float64[]
+    for (b, blk) in enumerate(blocks)
+        Xb, Yb = blk[1,:,:], blk[2,:,:]
+        Ny, Nz = size(Xb)
+        for i in 1:Ny, j in 1:Nz
+            push!(coords, Xb[i,j])
+            push!(coords, Yb[i,j])
+            push!(refs, PtRef(b,i,j))
+        end
+    end
+    coords = reshape(coords, (2, length(refs)))  # now 2×N_points
+    
+    tree = KDTree(coords)
+    return tree, refs
+end
+
 
 function find_nearest_kd(data, tree::KDTree, refs, xq, yq)
     idxs, dists = knn(tree, [xq,yq], 1)   # 1‐NN
     ref = refs[idxs[1]]
     blk = ref.block
     i,j = ref.i, ref.j
-    M11_val = data["M11"][blk][i, j]
-    M22_val = data["M22"][blk][i, j]
-    return [M11_val, M22_val]
+
+    if length(size(data["M11"][1])) == 3
+        M11_val = data["M11"][blk][i, j, 1]
+        M22_val = data["M22"][blk][i, j, 1]
+        return [M11_val, M22_val]
+    elseif length(size(data["M11"][1])) == 2
+        M11_val = data["M11"][blk][i, j]
+        M22_val = data["M22"][blk][i, j]
+        return [M11_val, M22_val]
+    else
+        error("Unsupported metric data format")
+    end
 end
